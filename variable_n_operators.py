90f252e6784862727462f03821e7a2be37f294de
Krishna <krishnaacharya.biratnagar@gmail.com>
2021-08-18 21:02:38 +0545

    variables and operatives practice!

---
+++ variable_n_operators.py
@@ -0,0 +1,24 @@
+print("Variables store certain values that can change ")
+
+x = 20
+y = 'This is new variable'
+print(x)
+print(y)
+x= 35
+print(x)
+# new value can be assigned to any variable and the newly assigned
+#will be its value
+
+
+a,b = 5,6
+
+print(a,b)
+#can also do multiple assignment
+# with variables separated by a comma.
+
+print("""Naming Requirements of Variables
+Variables in Python are required to start with a letter or an underscore.
+The rest of the name must consist of letters, numbers, or underscores
+Names are case-sensitive""")
+
+#printing multiple lines put """ lines """
