01edf22a45359e8fc418d19bfcbb41c128cd0610
Krishna <krishnaacharya.biratnagar@gmail.com>
2021-08-18 21:43:54 +0545

    (describe naming conventions and value assignment)

--- variable_n_operators.py
+++ variable_n_operators.py
@@ -22,3 +22,18 @@ The rest of the name must consist of letters, numbers, or underscores
 Names are case-sensitive""")
 
 #printing multiple lines put """ lines """
+
+print("""Naming Conventions
+Most variable names should be written in lower_snake_case. This means that all words should be lowercase, and words should be separated by an underscore.
+CAPITAL_SNAKE_CASE usually refers to constants
+UpperCamelCase usually refers to a class (more on that later)
+Variables that start and end with two underscores (called "dunder" for double underscore) are intended to be private or are builtins to the language""")
+
+myVariable = 3.14  # more like a JavaScript variable!
+my_variable = 3.14  # much better
+AVOGADRO_CONSTANT = 6.022140857 * 10 ** 23  # https://en.wikipedia.org/wiki/Avogadro_constant
+__no_touch__ = 3.14  # someone doesn't want you to mess with this!
+
+#To indicate an absence of value, variable can be assigned to the value None.
+
+nothing = None #here  variable 'nothing' doesnot have any value not even zero , 
