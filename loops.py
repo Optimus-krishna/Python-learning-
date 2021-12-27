a2ff9e9862363519d03880e30cef2b3b01e87720
Krishna <krishnaacharya.biratnagar@gmail.com>
2021-12-27 20:07:04 +0545

    add loops.py

---
+++ loops.py
@@ -0,0 +1,10 @@
+for i in range(6):
+    print(i)
+
+
+names = ["Rajesh Hamal", "Bhuwan K.C.", "Shiva Shrestha"]
+for name in names:
+    print(name)
+onename = "ChinnaSwamyMuthhaSwaamyBenuGopalAiyyar"
+for character in onename:
+    print(character)
