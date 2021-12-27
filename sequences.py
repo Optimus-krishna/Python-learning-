f23bfc09af6462a268076533eabac578e6eca094
Krishna <krishnaacharya.biratnagar@gmail.com>
2021-12-27 19:52:19 +0545

    add sequence.py

---
+++ sequences.py
@@ -0,0 +1,8 @@
+names = ["Rajesh Hamal", "Bhuwan K.C.", "Shiva Shrestha"]
+print(names)
+names.append("Nikhil Uprety")
+print(names)
+print(names[1][3])
+names.sort()
+print(names)
+cordinate = (10.0, 20.0)
